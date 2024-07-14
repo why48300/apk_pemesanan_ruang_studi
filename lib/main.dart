5736c4764e13ea825046196b160fccb8ba905b6a
why48300 <userpendosa@gmail.com>
2024-07-14 10:24:11 +0700

    update firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:pemesanan_ruang_studi/firebase_options.dart';
 import 'package:pemesanan_ruang_studi/splash_screen.dart';
 
 void main() async {
   await Hive.initFlutter();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(MyApp());
 }
